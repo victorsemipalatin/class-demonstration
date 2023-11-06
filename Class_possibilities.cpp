8fdd61f6af2816e929762e37281604883a5b2810
ooo <121344064+victorsemipalatin@users.noreply.github.com>
2023-11-06 16:03:41 +0300

    добавлены комментарии

--- Class_possibilities.cpp
+++ Class_possibilities.cpp
@@ -10,7 +10,7 @@
 using namespace std;
 
 
-class Parent_1{
+class Parent_1{ // родительский класс, в котором не были использованы виртуальные методы
   public:
     Parent_1(int x){
       if (x == 1)
@@ -26,7 +26,7 @@ class Parent_1{
 };
 
 
-class Child_1: public Parent_1{
+class Child_1: public Parent_1{ // дочерний класс, в котором переопределен метод print_sth()
   public:
     Child_1(int x): Parent_1(x){
       if (x == 1)
@@ -42,7 +42,7 @@ class Child_1: public Parent_1{
 };
 
 
-class Parent_2{
+class Parent_2{ // родительский класс, в котором были использованы виртуальные методы
   public:
     Parent_2(int x){
       if (x == 1)
@@ -58,7 +58,7 @@ class Parent_2{
 };
 
 
-class Child_2: public Parent_2{
+class Child_2: public Parent_2{ // дочерний класс, в котором метод print_sth() переопределён с помощью virtual
   public:
     Child_2(int x): Parent_2(x){
       if (x == 1)
@@ -73,11 +73,11 @@ class Child_2: public Parent_2{
 
 
 int main(){
-  // 1)
+  // 1) демонстрация порядка выполнения конструкторов между родительским классом и дочерним классами
   Child_1 child(1);
   cout << endl;
 
-  // 2.1)
+  // 2.1) демонстрация свойства полиморфизма с использованием переопределения
   Parent_1 p_1(0);
   Child_1 ch_1(0);
   Parent_1 *pointer_1 = &p_1;
@@ -86,7 +86,7 @@ int main(){
   pointer_1 -> print_sth();
   cout << endl;
 
-  // 2.2)
+  // 2.2) демонстраиция свойства полиморфизма с использованием виртуальной функции
   Parent_2 p_2(0);
   Child_2 ch_2(0);
   Parent_2 *pointer_2 = &p_2;
